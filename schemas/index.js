73fbf1518ab98e9e1cb5c4016bcfb719861349b4
Steve Mckellar <steve@wwqrd.uk>
2019-08-22 13:38:10 +0100

    v2 schema with INCLUDES/EXCLUDES

--- schemas/index.js
+++ schemas/index.js
@@ -1,9 +1,11 @@
 const version_1_0_0 = require('./1.0.0.js');
 const version_1 = require('./1.js');
+const version_2 = require('./2.js');
 
 const versions = [
   { version: '1.0.0', schema: version_1_0_0 },
   { version: '1', schema: version_1 },
+  { version: '2', schema: version_2 },
 ];
 
 module.exports = versions;
