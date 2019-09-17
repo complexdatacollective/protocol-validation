18d600bc1ea6fcc28c09e6c72f7749bac19f7e70
Steve Mckellar <steve@wwqrd.uk>
2019-09-17 15:12:23 +0100

    Add migration for version 2

--- migrations/migrations.js
+++ migrations/migrations.js
@@ -1,6 +1,7 @@
 const migrations = [
   { version: '1.0.0', migration: noop => noop },
   { version: '1', migration: noop => noop },
+  { version: '2', migration: noop => noop },
 ];
 
 module.exports = migrations;
