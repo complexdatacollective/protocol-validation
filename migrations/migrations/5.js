42884941dad16891befd49dea5fd8b7c2c0efaf6
Steve Mckellar <steve@wwqrd.uk>
2020-06-23 12:09:38 +0100

    nmtoken migration for entity type names

--- migrations/migrations/5.js
+++ migrations/migrations/5.js
@@ -9,31 +9,45 @@ const setProps = (props, source = {}) =>
       return { ...acc, [key]: props[key] };
     }, source);
 
-const getSafeValue = value => (
-  typeof value === 'string' ?
-    value.replace(/[\s]+/g, '_').replace(/[^a-zA-Z0-9._:-]+/g, '') :
-    value
-);
+const getNextSafeValue = (value, existing, inc = 1) => {
+  const incrementedValue = inc > 1 ? `${value}${inc}` : value;
+  if (!existing.includes(incrementedValue)) { return incrementedValue; }
+
+  return getNextSafeValue(value, existing, inc + 1);
+};
+
+const getSafeValue = (value, existing = []) => {
+  if (typeof value !== 'string') { return value; } // some option values are numeric
+
+  const safeValue = value.replace(/[\s]+/g, '_').replace(/[^a-zA-Z0-9._:-]+/g, '');
+
+  return getNextSafeValue(safeValue, existing);
+};
+
+const getNames = (obj = {}) =>
+  Object.keys(obj)
+    .map(key => obj[key].name);
 
 const migrateOptionValues = (options = []) =>
-  options.map(
-    ({ value, ...rest }) =>
-      ({ ...rest, value: getSafeValue(value) }),
-  );
+  options.reduce((acc, { value, ...rest }) => ([
+    ...acc,
+    { ...rest, value: getSafeValue(value, acc.map(o => o.value)) },
+  ]), []);
 
-const migrateVariable = (variable = {}) => setProps({
+const migrateVariable = (variable = {}, acc = {}) => setProps({
   options: migrateOptionValues(variable.options),
-  name: getSafeValue(variable.name),
+  name: getSafeValue(variable.name, getNames(acc)),
 }, variable);
 
 const migrateVariables = (variables = {}) =>
   Object.keys(variables)
     .reduce((acc, variableId) => ({
       ...acc,
-      [variableId]: migrateVariable(variables[variableId]),
+      [variableId]: migrateVariable(variables[variableId], acc),
     }), {});
 
-const migrateType = (type = {}) => setProps({
+const migrateType = (type = {}, acc = {}) => setProps({
+  name: getSafeValue(type.name, getNames(acc)),
   variables: migrateVariables(type.variables),
 }, type);
 
@@ -41,7 +55,7 @@ const migrateTypes = (types = {}) =>
   Object.keys(types)
     .reduce((acc, typeId) => ({
       ...acc,
-      [typeId]: migrateType(types[typeId]),
+      [typeId]: migrateType(types[typeId], acc),
     }), {});
 
 const migration = (protocol) => {
--- migrations/migrations/__tests__/5.test.js
+++ migrations/migrations/__tests__/5.test.js
@@ -5,44 +5,89 @@ const version5 = require('../5');
 const v4protocol = {
   codebook: {
     node: {
-      exampleNodeType: {
+      disallowedType: {
+        name: 'disallowed type name',
         variables: {
           invalidExampleVariable: {
             name: 'variable (with) disallowed characters!',
             options: [
               { label: 'foo', value: 'f o o!' },
+              { label: 'foo2', value: 'f o o?' }, // should resolve to the same as previous, therefore needs incrementing
               { label: 'bar', value: 'b_a-r:.' },
               { label: 'bazz', value: 5 },
             ],
+          },
+          invalidExampleVariableDuplicate: {
+            // should resolve to the same as previous, therefore needs incrementing
+            name: 'variable (with) disallowed characters?',
           },
           validExampleVariable: {
             name: 'variable_with-allowed:characters.',
           },
         },
       },
+      disallowedTypeDuplicate: {
+        // should resolve to the same as previous, therefore needs incrementing
+        name: 'disallowed_type name',
+      },
+      allowedTypeName: {
+        name: 'okay_-type:.name',
+      },
+    },
+    edge: {
+      edgeType: {
+        name: 'bar bazz',
+        variables: {
+          buzz: { name: 'fizz pop' },
+        },
+      },
+    },
+    ego: {
+      variables: {
+        foo: { name: 'foo bar' },
+      },
     },
   },
 };
 
 describe('migrate v4 -> v5', () => {
+  it('migrates codebook', () => {
+    const result = version5.migration(v4protocol);
+
+    expect(result).toMatchSnapshot();
+  });
+
+  it('type names', () => {
+    const result = version5.migration(v4protocol);
+
+    const nodeDefinitions = result.codebook.node;
+
+    expect(nodeDefinitions.disallowedType.name).toBe('disallowed_type_name');
+    expect(nodeDefinitions.disallowedTypeDuplicate.name).toBe('disallowed_type_name2');
+    expect(nodeDefinitions.allowedTypeName.name).toBe('okay_-type:.name');
+    expect(nodeDefinitions).toMatchSnapshot();
+  });
+
   it('variable names', () => {
     const result = version5.migration(v4protocol);
 
-    const variables = result.codebook.node.exampleNodeType.variables;
+    const variables = result.codebook.node.disallowedType.variables;
     expect(variables.invalidExampleVariable.name).toBe('variable_with_disallowed_characters');
+    expect(variables.invalidExampleVariableDuplicate.name).toBe('variable_with_disallowed_characters2');
     expect(variables.validExampleVariable.name).toBe('variable_with-allowed:characters.');
-    expect(result).toMatchSnapshot();
+    expect(variables).toMatchSnapshot();
   });
 
   it('option values', () => {
     const result = version5.migration(v4protocol);
 
-    const options = result.codebook.node.exampleNodeType.variables.invalidExampleVariable.options;
+    const options = result.codebook.node.disallowedType.variables.invalidExampleVariable.options;
     expect(options).toEqual(expect.arrayContaining([
       expect.objectContaining({ value: 'f_o_o' }),
+      expect.objectContaining({ value: 'f_o_o2' }),
       expect.objectContaining({ value: 'b_a-r:.' }),
       expect.objectContaining({ value: 5 }),
     ]));
-    expect(result).toMatchSnapshot();
+    expect(options).toMatchSnapshot();
   });
 });
