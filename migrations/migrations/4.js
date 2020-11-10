39530106da0b0b10848d8cbb6bd9695fa0c27814
Joshua Melville <joshua.melville@northwestern.edu>
2020-11-10 16:52:06 +0000

    Update 4.js
    Typos

--- migrations/migrations/4.js
+++ migrations/migrations/4.js
@@ -106,7 +106,7 @@ const notes = `
 - Add a numerical suffix (\`variable1\`, \`variable2\`, etc.) to any variables or categorical/ordinal values that clash as a result of these changes.
 - Rename node and edge types to ensure they are unique, and conform to the same requirements as variable names. Names that clash will get a numerical suffix, as above.
 - **NOTE:** If you are using external network data, you must ensure that you update your column headings manually to meet the same requirements regarding variable names outlined above. See our revised [documentation on variable naming](https://documentation.networkcanvas.com/docs/key-concepts/variable-naming/).
-- Remove any non-boolean 'additional variables' from prompts. It was neccesary to simplify this feature, and so only boolean variable types will be supported moving forwards. Any non-boolean variables you created that will be removed by this migration will remain in your codebook, but will be marked 'unused'. You should review and remove this manually, or replace them with equivalent boolean variables.`;
+- Remove any non-boolean 'additional variables' from prompts. It was necessary to simplify this feature, and so only boolean variable types will be supported moving forwards. Any non-boolean variables you created that will be removed by this migration will remain in your codebook, but will be marked 'unused'. You should review and remove these manually, or replace them with equivalent boolean variables.`;
 
 const v4 = {
   version: 4,
