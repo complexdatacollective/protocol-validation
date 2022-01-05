c2b240ac3f64cc7915ea8426d02de902211385b3
Joshua Melville <joshua.melville@northwestern.edu>
2022-01-05 11:47:17 +0200

    change tense of migration notes

--- migrations/migrations/5.js
+++ migrations/migrations/5.js
@@ -6,9 +6,9 @@ const migration = (protocol) => protocol;
 
 // Markdown format
 const notes = `
-- Enables the 'Tie Strength Census' interface, which allows you to conduct a dyad census that also captures the strength of the tie and assigns it to an ordinal variable
-- Adds new validation options for form fields: \`unique\`, \`sameAs\`, and \`differentFrom\`.
-- Enables an 'Interview Script' section for each stage, where notes for the interviewer can be added. 
+- Enable the 'Tie Strength Census' interface, which will allow you to conduct a dyad census that also captures the strength of the tie and assigns it to an ordinal variable.
+- Add new validation options for form fields: \`unique\`, \`sameAs\`, and \`differentFrom\`.
+- Enable an 'Interview Script' section for each stage, where notes for the interviewer can be added. 
 `;
 
 const v5 = {
