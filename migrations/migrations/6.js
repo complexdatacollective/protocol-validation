29f0c5c3db305255598bbb7bbe3d755a0b1b3ab3
Joshua Melville <joshua.melville@northwestern.edu>
2022-01-19 13:51:41 +0200

    Add migration that removes old roster interfaces, and update schema to reflect new NameGeneratorRoster interface

--- migrations/migrations/6.js
+++ migrations/migrations/6.js
@@ -2,10 +2,28 @@
  * Migration from v5 to v6
  */
 
-const migration = (protocol) => protocol;
+const migrateStages = (stages = []) => stages.map(stage => {
+
+  if (stage.type !== 'NameGeneratorAutocomplete' && stage.type !== 'NameGeneratorList') {
+    return stage;
+  }
+
+  return {
+    ...stage,
+    type: 'NameGeneratorRoster',
+  }
+});
+
+const migration = (protocol) => {
+  return {
+    ...protocol,
+    stages: migrateStages(protocol.stages),
+  }
+}
 
 // Markdown format
 const notes = `
+- Replace roster-based name generators (small and large) with a single new interface that combines the functionality of both.
 - Enable support for using the automatic node positioning feature on the Sociogram interface.
 `;
 
