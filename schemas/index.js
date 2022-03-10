de16e847fb7754471d6341d6e97bf6477c3abd2e
Joshua Melville <joshua.melville@northwestern.edu>
2022-03-10 15:54:37 +0200

    add minNodes and maxNodes to behaviours

--- schemas/index.js
+++ schemas/index.js
@@ -4,6 +4,7 @@ const version_3 = require('./3.js');
 const version_4 = require('./4.js');
 const version_5 = require('./5.js');
 const version_6 = require('./6.js');
+const version_7 = require('./7.js');
 
 const versions = [
   { version: 1, validator: version_1 },
@@ -12,6 +13,7 @@ const versions = [
   { version: 4, validator: version_4 },
   { version: 5, validator: version_5 },
   { version: 6, validator: version_6 },
+  { version: 7, validator: version_7 },
 ];
 
 module.exports = versions;
